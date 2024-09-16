885b704568a287acb311b5cc1855fc05921bd2c0
Olga Romanova <olgaromanova@MacBook-Pro-Olga.local>
2024-09-16 10:30:14 +0200

    Semantic HTML

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <img src="/icons_assets/Logo.svg" alt="Little Lemon Logo" />
+      <h1>Little Lemon Restaurant</h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReservationForm from "./ReservationForm";
+
+function Main() {
+  return (
+    <main>
+      <h1>Reserve a Table</h1>
+      <ReservationForm />
+    </main>
+  );
+}
+
+export default Main;
